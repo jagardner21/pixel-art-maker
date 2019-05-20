9d53edae26f4a40f1e1f907a947251403fa76e3c
jagardner21 <42302459+jagardner21@users.noreply.github.com>
2019-05-19 22:38:26 -0700

    added Clear button functionality, alignment improvements, current color indicator added

--- create-grid.js
+++ create-grid.js
@@ -6,9 +6,9 @@ function createGrid(height, width) {
         newRow.classList.add('row');
         for (let j = 0; j < width; j++) {
             var newSquare = document.createElement('div');
-            newSquare.classList.add('square')
+            newSquare.classList.add('square');
             newRow.appendChild(newSquare);
-
+            
         }
         grid.appendChild(newRow);
     }
@@ -21,17 +21,16 @@ let currentColor = '';
 function changeColor(event) {
     event.srcElement.style.backgroundColor = currentColor;
     event.srcElement.style.border = `${currentColor}`
-
+    
 }
 
 
 function selectColor(event) {
     circleClasses = event.srcElement.className;
     currentColor = circleClasses.split(' ')[1];
-    console.log(currentColor)
-    console.log(event);
+    document.getElementById("currentColor").style.backgroundColor = currentColor;
 }
 
 function refresh() {
-    //for "Clear" button
+    location.reload();
 }
